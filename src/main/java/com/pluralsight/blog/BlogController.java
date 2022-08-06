45c3f1b360289e5e162b3b873fa47dbc65965b75
drvg5 <dorove55@gmail.com>
2022-08-06 10:18:44 -0400

    Completed Module 2

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,14 +1,25 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class BlogController {
 
+    @Autowired
+    PostRepository postRepository;
+
     @RequestMapping("/")
     public String listPosts(ModelMap modelMap){
+
+        List<Post> listOfPosts = postRepository.getAllPosts();
+        modelMap.put("posts",listOfPosts);
         modelMap.put("title","Blog Post 1");
         return "home";
     }
